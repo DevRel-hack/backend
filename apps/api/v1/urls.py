9c41eb68300436d63db9c6c45eca8658b3d53897
Ruslan Atarov <ruslanatarov@gmail.com>
2023-12-14 12:37:00 +0300

    added api endpoints

--- apps/api/v1/urls.py
+++ apps/api/v1/urls.py
@@ -1,12 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .users.views import UserViewSet
+from .users.views import UserViewset
 
 app_name = "api"
 
 router = routers.DefaultRouter()
-router.register("users", UserViewSet, basename="users")
+router.register("users", UserViewset, basename="users")
 
 urlpatterns = [
     path("auth/", include("djoser.urls.jwt")),
