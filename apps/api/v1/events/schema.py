7b71476e4947e0ee664b5c3c074388a052f56a65
Ruslan Atarov <ruslanatarov@gmail.com>
2023-12-16 13:35:30 +0300

    fixed schema event

--- apps/api/v1/events/schema.py
+++ apps/api/v1/events/schema.py
@@ -15,18 +15,53 @@ request_sample = {
     "comments": "Комментарии",
 }
 
+response_sample = {
+    "status": {"id": 1, "title": "Подготовка"},
+    "title": "Новый Хакатон",
+    "description": "Много слов о мероприятии",
+    "start_at": "16.12.2023 18:00",
+    "is_internal": False,
+    "form": "on",
+    "place": "интернет",
+    "url": "https://linktoevent.com",
+    "tags": [{"id": 1, "title": "Python"}],
+    "comments": "Комментарии",
+}
+
 events_schema = {
-    "list": extend_schema(summary="Список мероприятий"),
-    "retrieve": extend_schema(summary="Просмотр мероприятия по ID"),
+    "list": extend_schema(
+        summary="Список мероприятий",
+        examples=[
+            OpenApiExample(
+                "Ответ",
+                value=[response_sample],
+                response_only=True,
+            ),
+        ],
+    ),
+    "retrieve": extend_schema(
+        summary="Просмотр мероприятия по ID",
+        examples=[
+            OpenApiExample(
+                "Ответ",
+                value=response_sample,
+                response_only=True,
+            ),
+        ],
+    ),
     "create": extend_schema(
         summary="Добавление нового мероприятия",
         examples=[
             OpenApiExample(
-                "Valid example 1",
-                summary="short summary",
+                "Запрос",
                 description="longer description",
                 value=request_sample,
                 request_only=True,
+            ),
+            OpenApiExample(
+                "Ответ",
+                value=response_sample,
+                response_only=True,
             ),
         ],
     ),
